14a3ad1848cfb068af086aeb5c5a97e21b544100
JDXE22 <davidesparzaj22@gmail.com>
2024-11-21 21:17:34 -0500

    Agregue el dropschema

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -4,15 +4,15 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 dotenvConfig({ path: '.env.development' });
 
-const config = {
+const config: DataSourceOptions = {
   type: 'postgres',
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT as unknown as number,
   username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  autoLoadEntities: true,
+  password: process.env.DB_PASSWORD,  
   synchronize: true,
+  dropSchema: true,
   entities: ['dist/**/*.entity{.ts, .js}'],
   migrations: ['dist/migrations/*{.js, .ts}']
 };
